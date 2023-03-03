e268c3156fc5f8a6d4fe9b00c0a8cdeb45fdcbaa
todpig <luccasraffael6@gmail.com>
2023-03-03 15:53:00 -0300

    componente que exclui comentarios e emit enviar sem ter texto

--- estudosReact/ReactRocketseat/fundamentos_react/src/components/comment.jsx
+++ estudosReact/ReactRocketseat/fundamentos_react/src/components/comment.jsx
@@ -2,8 +2,16 @@ import styles from "./comment.module.css";
 import { Trash } from "phosphor-react";
 import { ThumbsUp } from "phosphor-react";
 import { Avatar } from "./avatar";
+import { useState } from "react";
 
-export function Comment({content}) {
+export function Comment({ content, deleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+  function handleDeleteComment() {
+    deleteComment(content);
+  }
+  function handleLikeComent() {
+    setLikeCount(likeCount + 1);
+  }
   return (
     <div className={styles.comment}>
       <Avatar
@@ -17,7 +25,7 @@ export function Comment({content}) {
               <strong>Lucas Rafael</strong>
               <time title="03 de fevereiro as 09H">Cerca de 1h</time>
             </div>
-            <button title="Deletar comentario">
+            <button onClick={handleDeleteComment} title="Deletar comentario">
               <Trash size={20} />
             </button>
           </header>
@@ -25,9 +33,9 @@ export function Comment({content}) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComent}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
