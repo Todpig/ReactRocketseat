0469a3a63721d7d8619eeb15c8b1029c2d6af0c9
todpig <luccasraffael6@gmail.com>
2023-03-03 17:23:23 -0300

    falatava um espaçamento no header kkkk

--- estudosReact/ReactRocketseat/fundamentos_react_tsx/src/components/header.tsx
+++ estudosReact/ReactRocketseat/fundamentos_react_tsx/src/components/header.tsx
@@ -5,6 +5,7 @@ export function Header() {
   return (
     <header className={style.header}>
       <img src={ignitelogo} alt="logotipo do ignite project" />
+      <h1>Ignite Feed</h1>
     </header>
   );
 }
